e924e5a274a402ea54f5573f9c557b4a9ffd8fe8
Gordon Brüggemann <gb@albertbauerdigital.com>
2014-12-25 22:09:35 +0100

    update

--- recipes/default.rb
+++ recipes/default.rb
@@ -19,7 +19,7 @@
 #
 
 include_recipe "apache2"
-include_recipe "mysql::server"
+#include_recipe "mysql::server"
 include_recipe "database::mysql"
 include_recipe "php"
 include_recipe "php::module_mysql"
